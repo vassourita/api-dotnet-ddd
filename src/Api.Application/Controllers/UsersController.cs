67f436f08fe994d42c17db2153d2dc36e2a06179
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-10 16:07:42 -0300

    Created user controller

---
+++ src/Api.Application/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException err)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, err.Message);
+            }
+        }
+    }
+}
