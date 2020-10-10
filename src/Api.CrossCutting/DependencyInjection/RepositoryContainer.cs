28199a4d04af44e8baf01730aee3ac6284900915
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-10 16:29:14 -0300

    Added db context resolver on di container

--- src/Api.CrossCutting/DependencyInjection/RepositoryContainer.cs
+++ src/Api.CrossCutting/DependencyInjection/RepositoryContainer.cs
@@ -1,7 +1,7 @@
+using Api.Data.Context;
 using Api.Data.Repository;
 using Api.Domain.Interfaces;
-using Api.Domain.Interfaces.Services.User;
-using Api.Service.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.CrossCutting.DependencyInjection
@@ -11,6 +11,10 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureRepositoryDependencies(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            serviceCollection.AddDbContext<MyContext>(
+                opt => opt.UseMySql("Server=localhost;Port=4001;Database=api_dotnet_ddd;Uid=root;Pwd=docker")
+            );
         }
     }
 }
