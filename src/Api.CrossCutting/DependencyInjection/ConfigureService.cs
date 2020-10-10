c8cf7e5f306d2081a6f1112e1cb0443c04cc2d13
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-10 16:15:39 -0300

    Added di container for services

---
+++ src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -0,0 +1,14 @@
+using Api.Domain.Interfaces.Services.User;
+using Api.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependencyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IUserService, UserService>();
+        }
+    }
+}
