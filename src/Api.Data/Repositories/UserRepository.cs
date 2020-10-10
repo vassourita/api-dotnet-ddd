d2b14854c9915253c406590c2263e3011ca6464f
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-10 18:14:40 -0300

    Added efcore implementation for user repository

--- src/Api.Data/Repositories/UserRepository.cs
+++ src/Api.Data/Repositories/UserRepository.cs
@@ -1,10 +1,30 @@
+using System;
+using System.Threading.Tasks;
 using Api.Data.Context;
 using Api.Domain.Entities;
+using Api.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data.Repositories
 {
-    public class UserRepository : BaseRepository<UserEntity>
+    public class UserRepository : BaseRepository<UserEntity>, IUserRepository
     {
-        public UserRepository(MyContext context) : base(context) { }
+        private DbSet<UserEntity> _DataSet;
+        public UserRepository(MyContext context) : base(context)
+        {
+            _DataSet = _Context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> SelectAsync(string email)
+        {
+            try
+            {
+                return await _DataSet.SingleOrDefaultAsync(x => x.Email.Equals(email));
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
     }
 }
