e9b62c1fea49633be24303a24417ab7692b9e80f
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-10 14:05:51 -0300

    Created custom efcore data context

---
+++ src/Api.Data/Context/MyContext.cs
@@ -0,0 +1,17 @@
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
