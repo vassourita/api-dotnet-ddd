041eaf0f93e21371e7d34939ee9b7870792ec8b8
Vinicius Vassao <vlviniciusguaruja7@gmail.com>
2020-10-10 16:52:19 -0300

    Added post method to user controller

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,8 +11,14 @@ namespace Api.Application.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserService _Service;
+        public UsersController(IUserService service)
+        {
+            _Service = service;
+        }
+
         [HttpGet]
-        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        public async Task<ActionResult> Index()
         {
             if (!ModelState.IsValid)
             {
@@ -20,7 +27,52 @@ namespace Api.Application.Controllers
 
             try
             {
-                return Ok(await service.GetAll());
+                return Ok(await _Service.GetAll());
+            }
+            catch (ArgumentException err)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, err.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> Show(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _Service.Get(id));
+            }
+            catch (ArgumentException err)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, err.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _Service.Post(user);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetWithId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
             catch (ArgumentException err)
             {
